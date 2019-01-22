1090e6d8d87b3b84e80f5c1dad4ef03f37e3099f
lsamano <leizl.samano@gmail.com>
2019-01-22 11:28:13 -0500

    merged image upload with comments branch to master

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_01_22_145221) do
-=======
-ActiveRecord::Schema.define(version: 2019_01_22_144814) do
->>>>>>> adding_image_upload
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
