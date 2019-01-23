8252558605b4ae4dca95dece1d5280dd7194f817
lsamano <leizl.samano@gmail.com>
2019-01-23 11:47:05 -0500

    Added Follow functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
   has_one_attached :profile_pic
   has_many :liked_posts, through: :likes, source: :post
   has_many :commented_posts, through: :comments, source: :post
+
+  has_many :follows
+
+  # :following_follows = The follow that you are giving to someone else
+  # :class_name = The actual name of the class to be searched
+  # :foreign_key = The actual foreign_key on the table to be compared to a User's id
+  has_many :following_follows, class_name: "Follow", foreign_key: :follower_id
+  has_many :followeds, through: :following_follows, source: :followed
+
+  ##############################
+  has_many :followed_follows, class_name: "Follow", foreign_key: :followed_id
+  has_many :followers, through: :followed_follows, source: :follower
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_172812) do
+ActiveRecord::Schema.define(version: 2019_01_23_153316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2019_01_22_172812) do
     t.integer "user_id"
     t.integer "post_id"
     t.string "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
