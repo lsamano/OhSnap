f47288d328084ade5063bce44f1dd0b0619c3db4
lsamano <leizl.samano@gmail.com>
2019-01-25 11:42:38 -0500

    Added a timeline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_153316) do
+ActiveRecord::Schema.define(version: 2019_01_25_163146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2019_01_23_153316) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_categories", force: :cascade do |t|
